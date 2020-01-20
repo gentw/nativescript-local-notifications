533be03dd7cdcf93b72ea809bc42cc2e7f41ec00
gentw <gentiannuka@gmail.com>
2020-01-20 02:19:58 +0100

    creating time interval for specified number of notifications per day, fixing problem between hours for accuracy

--- demo-vue/app/components/App.vue
+++ demo-vue/app/components/App.vue
@@ -4,7 +4,7 @@
 
     <StackLayout>
       <Button @tap="onTapHasPermission" class="btn" text="Has Permission?"></Button>
-      <Button @tap="onTapScheduleNotification" class="btn" text="Schedule Notification1"></Button>
+      <Button @tap="onTapScheduleNotification" class="btn" text="Schedule Notification"></Button>
       <Button @tap="onTapCancelAll" class="btn" text="Cancel notifications"></Button>
       <Label class="message" :text="message" textWrap="true"></Label>
     </StackLayout>
@@ -29,7 +29,7 @@
     data() {
       return {
         message: "Tap a button above..",
-        alertNumber: 2,
+        alertNumber: 4,
         localPrayers: [],
         prayers: 
 
@@ -45,6 +45,7 @@
               thumbnail: "https://2.bp.blogspot.com/-H_SZ3nAmNsI/VrJeARpbuSI/AAAAAAAABfc/szsV7_F609k/s200/emoji.jpg",
               forceShowWhenInForeground: true,
               channel: "vue-channel",
+              interval: "day",
               ticker: "Special ticker text for Vue (Android only)",
               at: new Date(new Date().getTime() + (0 * 1000)), // 5 seconds from now
               actions: [
@@ -73,6 +74,7 @@
               thumbnail: "https://2.bp.blogspot.com/-H_SZ3nAmNsI/VrJeARpbuSI/AAAAAAAABfc/szsV7_F609k/s200/emoji.jpg",
               forceShowWhenInForeground: true,
               channel: "vue-channel",
+              interval: "day",
               ticker: "Special ticker text for Vue (Android only)",
               at: new Date(new Date().getTime() + (0 * 1000)), // 5 seconds from now
               actions: [
@@ -102,6 +104,7 @@
               thumbnail: "https://2.bp.blogspot.com/-H_SZ3nAmNsI/VrJeARpbuSI/AAAAAAAABfc/szsV7_F609k/s200/emoji.jpg",
               forceShowWhenInForeground: true,
               channel: "vue-channel",
+              interval: "day",
               ticker: "Special ticker text for Vue (Android only)",
               at: new Date(new Date().getTime() + (0 * 1000)), // 5 seconds from now
               actions: [
@@ -130,6 +133,36 @@
               thumbnail: "https://2.bp.blogspot.com/-H_SZ3nAmNsI/VrJeARpbuSI/AAAAAAAABfc/szsV7_F609k/s200/emoji.jpg",
               forceShowWhenInForeground: true,
               channel: "vue-channel",
+              interval: "day",
+              ticker: "Special ticker text for Vue (Android only)",
+              at: new Date(new Date().getTime() + (0 * 1000)), // 5 seconds from now
+              actions: [
+                {
+                  id: "yes",
+                  type: "button",
+                  title: "Yes (and launch app)",
+                  launch: true
+                },
+                {
+                  id: "no",
+                  type: "button",
+                  title: "No",
+                  launch: false
+                }
+              ]
+            },
+            {
+            id: 5,
+              title: 'X4?',
+              subtitle: 'This poster is awesome!',
+              body: 'The big brown Vue app jumped over the lazy Angular app. The big brown Vue app jumped over the lazy Angular app. The big brown Vue app jumped over the lazy Angular app. The big brown Vue app jumped over the lazy Angular app.',
+              bigTextStyle: false, // Allow more than 1 row of the 'body' text on Android, but setting this to true denies showing the 'image'
+              color: new Color("green"),
+              image: "https://images-na.ssl-images-amazon.com/images/I/61mx-VbrS0L.jpg",
+              thumbnail: "https://2.bp.blogspot.com/-H_SZ3nAmNsI/VrJeARpbuSI/AAAAAAAABfc/szsV7_F609k/s200/emoji.jpg",
+              forceShowWhenInForeground: true,
+              channel: "vue-channel",
+              interval: "day",
               ticker: "Special ticker text for Vue (Android only)",
               at: new Date(new Date().getTime() + (0 * 1000)), // 5 seconds from now
               actions: [
@@ -176,9 +209,7 @@
 
 
         while(counter > 0) {
-         
           let index = Math.floor(Math.random() * counter); 
-
 
           counter--;
 
@@ -192,29 +223,55 @@
       },
 
       increaseSecForEach(array) {
-        // var prayers = this.shuffleArray(this.prayers);
-        // var seconds = 5;
-        // for(let i=0; i<this.alertNumber; i++) {
-        //   this.localPrayers.push(prayers[i]);
-        //   // this.localPrayers[i].at = new Date(new Date().getTime() + (seconds * 1000));
-        //   // seconds++;
-        // }
+       
         
-        let sec=5;
-        for(let i=0; i<this.alertNumber; i++) {          
-          array[i]['at'] = new Date(new Date().getTime() + (sec * 1000));
+        let date = new Date();
+        //let hour = date.getHours(); 
+        let hours = new Date().getHours();
+        let ampm = (hours >= 12) ? "PM" : "AM";
+     
+        let year = date.getFullYear();
+        let month = date.getMonth();
+        let day = date.getDate();
+        //let hour = date.getHours();
+        
+        // ktu e cakton prej ores sa me ardh notification
+        let hour = 6;
+        //let minute = 17;
+        let eachHour = 12 / this.alertNumber;
+        let varForCondition = eachHour;
+        
+        //let sec = 0;
+        let min = 0;
+        if(eachHour % 2 != 0) {
+            eachHour = Math.floor(eachHour);         
+        } 
+
+
+        for(let i=0; i<this.alertNumber; i++) {  
+          if(varForCondition % 2 == 0) {
+            //array[i]['at'] = new Date(new Date(year, month, day, hour, minute));
+            array[i]['at'] = new Date(new Date(year, month, day, hour));
+          } else {
+            
+            if(this.alertNumber != 2) {
+
+              if(i == this.alertNumber - 1) {
+                min += 30;
+              }  else if (i == 0) {
+                min = 0;
+              } else {
+                min += 30;
+              }          
+            } 
+
+            array[i]['at'] = new Date(new Date(year, month, day, hour, min));
+          }
           this.localPrayers.push(array[i]);
-          sec+=5;
+          alert(array[i]['at']);
+
+          hour+=eachHour;
         }
-
-        /*for(let i=0; i<this.localPrayers.length; i++) {
-          
-          alert(this.localPrayers[0]);
-        }*/
-
-        // return this.localPrayers;
-
-
       }, 
 
 
@@ -222,16 +279,10 @@
         var index = 1;
        
 
-        /*$.each(this.prayers, function(elem) {
-          if(localPrayers.length() <= this.alertNumber) {
-            localPrayers.push(elem);
-          }
-        });*/
-
-
-       this.shuffleArray(this.prayers);
+        this.shuffleArray(this.prayers);
 
         //alert(JSON.stringify(this.shuffleArray(this.prayers)));
+
       
 
 
